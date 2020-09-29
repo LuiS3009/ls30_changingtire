27ba14b8a9dfe736f5af768527fc92ef39885429
LuiS30 <72078928+LuiS3009@users.noreply.github.com>
2020-09-29 13:48:45 +0100

    Update main.lua

--- ls30_changingtire/server/main.lua
+++ ls30_changingtire/server/main.lua
@@ -4,16 +4,16 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 ESX.RegisterUsableItem('pneu', function(source)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
-	local chavef = xPlayer.getInventoryItem('chavematricula').count
+	local chavef = xPlayer.getInventoryItem('chavefendas').count
 	if chavef >= 1 then
-		TriggerClientEvent('ls30_ changingtire:usar', _source)
+		TriggerClientEvent('ls30_changingtire:usar', _source)
 	else
 		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Não tens nenhuma chave de fendas!'})
 	end
 end)
 
-RegisterNetEvent('ls30_ changingtire:remover')
-AddEventHandler('ls30_ changingtire:remover', function()
+RegisterNetEvent('ls30_changingtire:remover')
+AddEventHandler('ls30_changingtire:remover', function()
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 
