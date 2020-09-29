116e1690db84cbd1adbe40b9cf863aa81aec5733
LuiS30 <72078928+LuiS3009@users.noreply.github.com>
2020-09-30 00:05:01 +0100

    Update sql.sql

--- ls30_changingtire/sql.sql
+++ ls30_changingtire/sql.sql
@@ -1,3 +1,4 @@
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES 
 ('pneu', 'Pneu', '1', '0', '1'),
+('pneuestragado', 'Pneu estragado', '1', '0', '1'),
 ('chavefendas', 'Chave de Fendas', '1', '0', '1');
